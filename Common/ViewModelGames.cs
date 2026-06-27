3d0a322d4875a5616863f5852b1f174161a01ab2
iliai <iliai@DESKTOP-J4KBH97>
2026-06-27 17:04:08 +0500

    Модель представления данных. Яшин.

---
+++ Common/ViewModelGames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelGames
+    {
+        public Snakes SnakesPlayers = new Snakes();
+        public Snakes.Point Points = new Snakes.Point();
+        public int IdSnake {  get; set; }
+    }
+}
