5846f13f3a787221bc6bf0d048ecb5c05530ddc3
iliai <iliai@DESKTOP-J4KBH97>
2026-06-27 19:13:51 +0500

    Созданный метод Send. Яшин.

--- Snake_Yashin/Program.cs
+++ Snake_Yashin/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Common;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +16,45 @@ namespace Snake_Yashin
     {
         public static List<Leaders> Leaders = new List<Leaders>();
         public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
-        public static List<ViewModelGame> viewModelGames = new List<ViewModelGame>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         public static int LocalPort = 5001;
         public static int Speed = 15;
         static void Main(string[] args)
         {
+            foreach (ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IpAddress),
+                    int.Parse(User.Port));
+            }
+        }
+        private static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IpAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(
+                        viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IpAddress}:{User.Port}");
+                }
+                catch (Exception exp)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + exp.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
         }
     }
 }
