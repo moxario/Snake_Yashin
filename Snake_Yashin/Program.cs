7474be1438c93e3cd54aacf1efd66721c02a4a52
iliai <iliai@DESKTOP-J4KBH97>
2026-06-27 16:31:34 +0500

    Добавленные переменные сервера. Яшин.

--- Snake_Yashin/Program.cs
+++ Snake_Yashin/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Snake_Yashin
 {
-    internal class Program
+    class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGame> viewModelGames = new List<ViewModelGame>();
+        public static int LocalPort = 5001;
+        public static int Speed = 15;
         static void Main(string[] args)
         {
         }
