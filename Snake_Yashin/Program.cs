17d0d95ad66d6f99c9768e12f03a4d5d5c422951
iliai <iliai@DESKTOP-J4KBH97>
2026-06-27 19:42:45 +0500

    Созданный метод управления змеёй. Яшин.

--- Snake_Yashin/Program.cs
+++ Snake_Yashin/Program.cs
@@ -56,5 +56,61 @@ namespace Snake_Yashin
                 }
             }
         }
+       public static void Receiver()
+        {
+            UdpClient receivingUdpClient = new UdpClient();
+            IPEndPoint endPoint = null;
+            try
+            {
+                Console.WriteLine("Команды сервера: ");
+                while (true)
+                {
+                    byte[] receiverData = receivingUdpClient.Receive(ref endPoint);
+                    string returnData = Encoding.UTF8.GetString(receiverData);
+                    string[] dataMessage = returnData.Split('|');
+                    ViewModelUserSettings User = JsonConvert.DeserializeObject<ViewModelUserSettings>(dataMessage[1]);
+                    if (returnData.ToString().Contains("/start"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Подключился пользователь: {User.IpAddress}:{User.Port}");
+                        remoteIPAddress.Add(User);
+                        User.IdSnake = AddSnake();
+                        viewModelGames[User.IdSnake].IdSnake = User.IdSnake;
+                    }
+                    else
+                    {
+                        int IdPlayer = -1;
+                        IdPlayer = remoteIPAddress.FindIndex(x => x.IpAddress == User.IpAddress &&
+                        x.Port == User.Port);
+                        if (IdPlayer != -1)
+                        {
+                            if (dataMessage[0] == "Up" && 
+                                viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Down)
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Up;
+                            else if(dataMessage[0] == "Down" && 
+                                viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Up)
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Down;
+                            else if (dataMessage[0] == "Left" &&
+                               viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Right)
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Left;
+                            else if (dataMessage[0] == "Right" &&
+                               viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Left)
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Right;
+
+                        }
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Возникло исключение: " + exp.Message);
+            }
+        }
+
+        private static int AddSnake()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
